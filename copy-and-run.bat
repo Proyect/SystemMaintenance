349587c58f5d322fd8153d7e57b91e35d12c90a5
Proyect <arieldiaz.sistemas@gmail.com>
2025-03-10 19:55:16 -0300

    create log

--- copy-and-run.bat
+++ copy-and-run.bat
@@ -2,4 +2,5 @@ echo "Copiando Archivos"
 schtasks /create /tn "Clean System" /tr "C:\Users\system-cleaning.bat" /sc daily /st 22:30 /f
 Xcopy /y "D:\system-cleaning.bat" "C:\Users\system-cleaning.bat"
 cd "C:\Users\"
+echo Mantenimiento realizado el %date% a las %time% >> mantenimiento.txt
 system-cleaning.bat
